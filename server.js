637873b3f156bb103a38c7293d78267487505cc9
N-Desmarais <55117352+N-Desmarais@users.noreply.github.com>
2021-04-08 03:01:50 -0400

    https addition

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const express = require("express");
+const http = require("http");
+const http = require("https")
 const sequelize = require("./models/index.js");
 const views = __dirname + '/views/';
 
 const app = express();
-
-var corsOrigin = {
-  origin: "*"
-};
+const corsOrigin = { origin: "*" };
 
 
 
@@ -16,6 +15,10 @@ app.use(express.static(views))
 app.use(bodyParser.json());
 app.use(cors(corsOrigin));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use (function (req, res, next) { // redirect port 80 to 443
+  if (req.secure) next();
+  else res.redirect('https://' + req.headers.host + req.url);
+});
 
 sequelize.sync();
 
@@ -27,7 +30,17 @@ require("./routes/document.routes")(app);
 require("./routes/project.routes")(app);
 require("./routes/user.routes")(app);
 
-const PORT = 8080;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+var options = {
+  ca: fs.readFileSync('chain.pem'),
+  cert: fs.readFileSync('cert.pem', 'utf8'),
+  key: fs.readFileSync('privkey.pem', 'utf8')
+}
+
+setTimeout(function () {server.setSecureContext({
+  ca: fs.readFileSync('chain.pem'),
+  cert: fs.readFileSync('cert.pem', 'utf8'),
+  key: fs.readFileSync('privkey.pem', 'utf8')
+})},86400000)
+
+const server = http.createServer(app).listen(80);
+const serverSecure = https.createServer(options, app).listen(443);
